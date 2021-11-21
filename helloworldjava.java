f83d4425e03673bed8f1f298165345bda7eb8bcd
mohankumar-97 <64099669+mohankumar-97@users.noreply.github.com>
2021-11-21 12:51:28 +0530

    Update helloworldjava.java

--- helloworldjava.java
+++ helloworldjava.java
@@ -1,6 +1,6 @@
 public class helloworldjava {
    public static void main(String[] args) {
-    System.out.println("Hello World from DevOps! test of poll scm");
+    System.out.println("Hello World from DevOps! test of web hook");
     }
 }
 
